3141e31bcf0aa628365c1fd0ee18cdeadd8b1f6b
ReneeQuevedo <renee.lohaus@gmail.com>
2023-03-15 17:51:00 -0500

    march 15th homework correected

--- March-15-homework/script.js
+++ March-15-homework/script.js
@@ -1,12 +1,31 @@
 for (var i = 1; i <= 100; i++) {
 if (i %3 && i % 5) {
-        console.log (i + "FizzBuzz.");
+        console.log ("FizzBuzz.");
     } else if (i % 3 == 0) {
-        console.log(i + "Fizz.");
+        console.log("Fizz.");
     } else if (i % 5 == 0) {
-        console.log (i + "Buzz.");
-    } 
+        console.log ("Buzz.");
+    } else {
+        console.log(i);
+    }
 }
 console.log(i)
 
 console.log("This has ended");
+
+
+// in class 
+//print the number 1 to 100 but if the number is divisible by 3 print > Fizz and if the number is divisible by 5 > buzz and if it is divisible by both 3 & 5 > Fizzbuzz
+
+for (var i = 1; i <= 100; i++) {
+    if ( i % 3 ==0) {
+        if (i% 5 ==0) {
+            console.log("FizzBuzz")
+        }
+        else console.log ("Fizz");
+    } else if (i % 5 ==0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
