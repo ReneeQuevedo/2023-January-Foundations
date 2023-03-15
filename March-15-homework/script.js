3dc3265b4b66c27d855150d6077669dd8b528a16
ReneeQuevedo <renee.lohaus@gmail.com>
2023-03-15 16:31:42 -0500

    march 15th homework

--- March-15-homework/script.js
+++ March-15-homework/script.js
@@ -1,12 +1,12 @@
 for (var i = 1; i <= 100; i++) {
-	if (i % 3 == 0) {
-        console.log(i + "Fizz.");
-    } if (i % 5 == 0) {
-        console.log (i + "Buzz.");
-    } if (i %3 && i % 5) {
+if (i %3 && i % 5) {
         console.log (i + "FizzBuzz.");
-    } else {
-        console.log(i)
-    }
+    } else if (i % 3 == 0) {
+        console.log(i + "Fizz.");
+    } else if (i % 5 == 0) {
+        console.log (i + "Buzz.");
+    } 
 }
+console.log(i)
+
 console.log("This has ended");
