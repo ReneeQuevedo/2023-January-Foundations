cfa5c0fbe20a9880cebf39d607760bd1a512b7ac
ReneeQuevedo <renee.lohaus@gmail.com>
2023-04-24 21:10:11 -0500

    final assesment end-class

--- final-assessment/js/orange.js
+++ final-assessment/js/orange.js
@@ -14,33 +14,13 @@ closeBtn.addEventListener('click', closeModal);
 
 // function to open modal
 function openModal() {
-    modal.style.display = 'block';
+    modal.style.display = 'flex';
 }
 
 // function to close modal
 function closeModal() {
     modal.style.display = 'none';
 }
-
-
-//contact js
-
-// var id = null;
-// function myMove() {
-//     var elem = document.getElementById("simpleModalTwo");
-//     var pos = 0;
-//     clearInterval(id);
-//     id = setInterval(fram, 10);
-//     function fram () {
-//         if (pos == 350) {
-//             clearInterval(id);
-//         } else {
-//             pos++;
-//             elem.style.top = pos + 'px';
-//             elem.style.left = pos + 'px';
-//         }
-//     }
-// }
 
 
 // get modal element
@@ -57,14 +37,9 @@ closeBtnTwo.addEventListener('click', closeModalTwo);
 
 // function to open modal
 function openModalTwo() {
-    modalTwo.style.display = 'block';
+    modalTwo.style.display = 'flex';
 }
 // function to close modal
 function closeModalTwo() {
     modalTwo.style.display = 'none';
 }
-
-// window.addEventListener ('click', function() {
-//     var pageContent = document.querySelector('.page-content');
-//     pageContent.style.transform = 'translateY(0)';
-// });
